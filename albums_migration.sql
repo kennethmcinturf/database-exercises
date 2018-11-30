9e4a0e6c0c8199701b14d72332d12834d9df26d3
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-11-30 12:21:39 -0600

    Select exercise finished

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,13 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 create table albums(
-      id INT UNSIGNED NOT NULL AUTO_INCREMENT, artist_name VARCHAR(50),
-      album_name VARCHAR(100),
+      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      artist_name VARCHAR(50),
+      album_name VARCHAR(100) NOT NULL,
       release_date DATE,
-      sales DECIMAL (4,2),
-      genre VARCHAR (25),
+      sales DECIMAL (10,2),
+      genre VARCHAR (100) NOT NULL,
       PRIMARY KEY (id)
   );
+
 
