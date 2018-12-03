2fe841e25cd8fa187b3396648a5a64ce25cc0fa7
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-12-02 18:18:52 -0600

    delete excercise done

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -3,3 +3,5 @@ USE codeup_test_db;
 delete from albums where release_date > 1991;
 delete from albums where genre = 'disco';
 delete from albums where artist_name = 'Eagles';
+
+select Language,Percentage from countrylanguage where CountryCode = 'USA';
